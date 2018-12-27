8154b4b0a3b3e5ef6c0e8cc3a196a92a26f573c9
TechnicallyMay <masonwells01@gmail.com>
2018-12-26 21:48:25 -0700

    Added players list, will be used to find all players registered and create player objects.

--- overwatcher.py
+++ overwatcher.py
@@ -1,15 +1,22 @@
+import glob
+import os
 from pages import *
 from player import Player
 
-# plot_page = PlotPage("Plot", "p", [])
-# stat_page = StatPage("Enter Stats", "s", [])
-# player_page = PlayersPage("Players Menu", "m", [])
+
+player_names = [os.path.basename(file).replace(".txt", "")
+                    for file in glob.glob('./data/players/*.txt')]
+
+print(player_names)
+# plot_page = PlotPage("Plot", "p")
+# stat_page = StatPage("Enter Stats", "s")
+# player_page = PlayersPage("Players Menu", "m")
 # main_page = Page("Home", "h", [plot_page, stat_page, player_page])
 # main_page.show_options()
 
-mason = Player("Mason")
-
-for hero, stats in mason.hero_stats.items():
-    print("\n" + hero)
-    for key, stat in stats.items():
-        print(key, stat, end = " ")
+# mason = Player("Mason")
+#
+# for hero, stats in mason.hero_stats.items():
+#     print("\n" + hero)
+#     for key, stat in stats.items():
+#         print(key, stat, end = " ")
