01181e92de357f1965e253c968eb60ec5e177beb
TechnicallyMay <masonwells01@gmail.com>
2018-12-29 01:50:45 -0700

    More explicit with page classes.

--- overwatcher.py
+++ overwatcher.py
@@ -1,16 +1,13 @@
-from pages import *
+import pages
 
 
-plot_page = PlotPage("Plot", "p")
-player_page = PlayersPage("Players Menu", "m")
-stat_page = StatPage("Enter Stats", "s", [player_page])
-main_page = Page("Home", "h", [plot_page, stat_page, player_page])
+plot_page = pages.PlotPage("Plot", "p", "View Charts")
+player_page = pages.PlayersPage("Players Menu", "m", "Edit Players")
+stat_page = pages.StatPage("Enter Stats", "s", "Enter Stats", [player_page])
+main_page = pages.Page("Home", "h", None, [plot_page, stat_page, player_page])
 main_page.show_options()
 
 
-
-# mason = Player("Mason")
-#
 # for hero, stats in mason.hero_stats.items():
 #     print("\n" + hero)
 #     for key, stat in stats.items():
