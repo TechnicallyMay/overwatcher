d6f8d09a8941890586553874d6538f8f3570e9ae
TechnicallyMay <masonwells01@gmail.com>
2018-12-29 15:10:43 -0700

    Made menus make a bit more sense.

--- main/main.py
+++ main/main.py
@@ -1,9 +1,9 @@
 import pages
 
 
-plot_page = pages.PlotPage("Plot", "p", "View Charts")
+plot_page = pages.PlotPage("Plot Menu", "p", "View Stats")
 player_page = pages.PlayersPage("Players Menu", "m", "Edit Players")
-stat_page = pages.StatPage("Enter Stats", "s", "Enter Stats", [player_page])
+stat_page = pages.StatPage("Enter Stats Menu", "s", "Enter Stats", [player_page])
 main_page = pages.Page("Home", "h", None, [plot_page, stat_page, player_page])
 
 
