ae3cc1812446a44a5b3b502d8f20f17eb2fceb52
TechnicallyMay <masonwells01@gmail.com>
2018-12-29 15:39:44 -0700

    plot_page can now go back to player_page

--- main/main.py
+++ main/main.py
@@ -1,8 +1,8 @@
 import pages
 
 
-plot_page = pages.PlotPage("Plot Menu", "p", "View Stats")
 player_page = pages.PlayersPage("Players Menu", "m", "Edit Players")
+plot_page = pages.PlotPage("Plot Menu", "p", "View Stats", [player_page])
 stat_page = pages.StatPage("Enter Stats Menu", "s", "Enter Stats", [player_page])
 main_page = pages.Page("Home", "h", None, [plot_page, stat_page, player_page])
 
