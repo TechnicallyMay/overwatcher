1ad7118c86d78170bc6b6d9a7920ebd7e3500ff8
TechnicallyMay <masonwells01@gmail.com>
2018-12-30 23:45:21 -0700

    Added remainder of planned plots.

--- main/plot.py
+++ main/plot.py
@@ -3,89 +3,155 @@ import matplotlib.pyplot as plt
 
 class Plot():
 
-    def __init__(self, data, title):
-        self.data = data
+    def __init__(self, title):
         self.title = title
+        fig, self.ax = plt.subplots()
 
 
     def show(self):
+        self.set_axis_labels()
         plt.xlabel(self.xlabel)
         plt.ylabel(self.ylabel)
-        plt.legend()
+        plt.title(self.title)
         plt.show()
+
+
+    def set_axis_labels(self):
+        if "x_lim" in dir(self):
+            plt.xlim(self.x_lim)
+        if "y_lim" in dir(self):
+            plt.ylim(self.y_lim)
+        if "x_ticks" in dir(self):
+            self.ax.set_xticks(self.x_ticks)
+        if "x_labels" in dir(self):
+            self.ax.set_xticklabels(self.x_labels, rotation = 90)
+        if "y_ticks" in dir(self):
+            self.ax.set_yticks(self.y_ticks)
+        if "y_labels" in dir(self):
+            self.ax.set_yticklabels(self.y_labels)
 
 
 class SRPlot(Plot):
 
-    def __init__(self, data, title, players):
-        super().__init__(data, title)
+    def __init__(self, title):
+        super().__init__(title)
         self.xlabel = "Game"
         self.ylabel = "SR"
-        self.players = players
-        self.get_lims()
+        self.x_lim = (0, 0)
+        self.y_lim = (7000, 0)
+        self.y_ticks = [0, 1500, 2000, 2500, 3000, 3500, 4000]
 
 
-    def get_lims(self):
-        all_sr_values = [sr for list in self.data for sr in list]
-        max_sr = max(all_sr_values)
-        min_sr = min(all_sr_values)
-        longest = max([len(list) for list in self.data])
-        self.y_lim = (min_sr - 500, max_sr + 500)
-        self.x_lim = (0, longest + 2)
+    def check_limits(self, data):
+        data_lim_top = max(data) + 500
+        data_lim_bottom = min(data) - 500
+        if data_lim_top > self.y_lim[1]:
+            self.y_lim = (self.y_lim[0], data_lim_top)
+        if data_lim_bottom < self.y_lim[0]:
+            self.y_lim = (data_lim_bottom, self.y_lim[1])
+        if len(data) > self.x_lim[1]:
+            self.x_lim = (0, len(data) + 2)
+
+
+    def add(self, data, name):
+        self.ax.plot(range(len(data)),
+                     data,
+                     label = name,
+                     marker = "."
+                     )
+        self.check_limits(data)
 
 
     def build(self):
-        fig, ax = plt.subplots()
-        for i, set in enumerate(self.data):
-            ax.plot(range(len(set)),
-                    set,
-                    label = self.players[i].name,
-                    marker = ".")
-        ax.set(xlabel = self.xlabel, ylabel = self.ylabel,
-               title = self.title)
-        y_ticks = [0, 1500, 2000, 2500, 3000, 3500, 4000]
-        ax.set_yticks(y_ticks)
-        self.fill_rank_colors(y_ticks)
-        plt.xlim(self.x_lim)
-        plt.ylim(self.y_lim)
+        self.fill_rank_colors()
+        plt.legend()
 
 
-    def fill_rank_colors(self, start_ys):
+    def fill_rank_colors(self):
         rank_colors = ["#cd7f32", "#D3D3D3", "#DAA520",
                        "#A0AABF", "#9ac5db", "#ff80ff",
                        "#ff8080"]
-        x_vals = [i for i in range(self.x_lim[1] + 5)]
-        for i in range(len(start_ys)):
-            y1_vals = [start_ys[i]] * len(x_vals)
+        x_vals = range(self.x_lim[1] + 5)
+        for i in range(len(self.y_ticks)):
+            y1_vals = [self.y_ticks[i]] * len(x_vals)
             try:
-                y2_vals = [start_ys[i + 1]] * len(x_vals)
+                y2_vals = [self.y_ticks[i + 1]] * len(x_vals)
             except IndexError:
                 y2_vals = [5000] * len(x_vals)
-            plt.fill_between(x_vals, y1_vals,
-                            y2_vals, color = rank_colors[i],
+            plt.fill_between(x_vals, y1_vals, y2_vals,
+                             color = rank_colors[i],
                              alpha = .4)
-
-
-class SRByHero(Plot):
-
-    def __init__(self, data, title):
-        super().__init__(data, title)
-        self.xlabel = "Hero"
-        self.ylabel = "Average Change in SR"
 
 
 class WinLossByHero(Plot):
 
-    def __init__(self, data, title):
-        super().__init__(data, title)
+    def __init__(self, title):
+        super().__init__(title)
         self.xlabel = "Hero"
         self.ylabel = "Win / Loss Ratio"
+        self.y_lim = (0, 1)
+        self.win_bars = []
+        self.loss_bars = []
+        self.x_labels = []
+
+
+    def add(self, win_loss, hero):
+        self.win_bars.append(win_loss)
+        self.loss_bars.append(1 - win_loss)
+        self.x_labels.append(hero)
+
+
+    def build(self):
+        num_of_bars = range(len(self.win_bars))
+        self.x_ticks = num_of_bars
+        self.ax.bar(num_of_bars, self.win_bars, color = "green")
+        self.ax.bar(num_of_bars, self.loss_bars, color = "red",
+                    bottom = self.win_bars)
+
+
+class SRByHero(Plot):
+
+    def __init__(self, title):
+        super().__init__(title)
+        self.xlabel = "Hero"
+        self.ylabel = "Average Change in SR"
+        self.bars = []
+        self.x_labels = []
+        self.x_ticks = []
+
+
+    def add(self, change, hero):
+        self.bars.append(change)
+        self.x_labels.append(hero)
+
+
+    def build(self):
+        num_of_bars = range(len(self.bars))
+        self.x_ticks = num_of_bars
+        self.ax.bar(num_of_bars, self.bars, width = 1,
+                    edgecolor = "black")
+        plt.grid(axis = "y", alpha = 0.5)
 
 
 class PerformancePlot(Plot):
 
-    def __init__(self, data, title):
-        super().__init__(data, title)
+    def __init__(self, title):
+        super().__init__(title)
         self.xlabel = "Change in SR"
         self.ylabel = "Performance"
-        self.players = players
+        self.xlim = (-30, 30)
+        self.ylim = (-1, 11)
+        self.x_ticks = range(-30, 35, 10)
+        self.x_labels = ["-30", "-20", "-10", "0", "+10", "+20", "+30"]
+        self.y_ticks = range(11)
+
+
+    def add(self, change, performance, name):
+        self.ax.scatter(change,
+                        performance,
+                        label = name,
+                        )
+
+
+    def build(self):
+        plt.legend()
