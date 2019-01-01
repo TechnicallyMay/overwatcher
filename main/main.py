78f87c9443991ce1d78224c93b71e3a2ff7a62f3
TechnicallyMay <masonwells01@gmail.com>
2018-12-31 19:53:53 -0700

    Changed wording

--- main/main.py
+++ main/main.py
@@ -2,7 +2,7 @@ import pages
 
 
 player_page = pages.PlayersPage("Players Menu", "m", "Edit Players")
-plot_page = pages.PlotPage("Plot Menu", "p", "View Stats", [player_page])
+plot_page = pages.PlotPage("Plot Menu", "p", "View Plots", [player_page])
 stat_page = pages.StatPage("Enter Stats Menu", "s", "Enter Stats", [player_page])
 main_page = pages.Page("Home", "h", None, [plot_page, stat_page, player_page])
 if __name__ == "__main__":
